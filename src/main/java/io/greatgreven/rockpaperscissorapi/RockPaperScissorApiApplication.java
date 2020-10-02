8ddf63467e98692de99f286112823b5b3beca10a
Eric Grevillius <ericgrevillius@gmail.com>
2020-10-02 20:02:01 +0200

    gradle project init

---
+++ src/main/java/io/greatgreven/rockpaperscissorapi/RockPaperScissorApiApplication.java
@@ -0,0 +1,13 @@
+package io.greatgreven.rockpaperscissorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorApiApplication.class, args);
+	}
+
+}
