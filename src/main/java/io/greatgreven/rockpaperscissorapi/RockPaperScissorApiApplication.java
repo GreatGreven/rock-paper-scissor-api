f74ed82e0180bf2414a5f843886913d09b0b61ca
Eric Grevillius <34370827+GreatGreven@users.noreply.github.com>
2023-01-25 20:12:45 +0100

    Added swagger docs

--- src/main/java/io/greatgreven/rockpaperscissorapi/RockPaperScissorApiApplication.java
+++ src/main/java/io/greatgreven/rockpaperscissorapi/RockPaperScissorApiApplication.java
@@ -1,13 +1,18 @@
 package io.greatgreven.rockpaperscissorapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Roshambo API", version = "1.0", description = "The game of roshambo through an API"
+))
 public class RockPaperScissorApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RockPaperScissorApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RockPaperScissorApiApplication.class, args);
+    }
 
 }
